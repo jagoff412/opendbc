79ed89a5331cd971950f78dbf71eea22978aee3c
jagoff2 <taystaff.011@gmail.com>
2025-05-23 18:20:07 -0400

    steer

--- opendbc/safety/safety/safety_hyundai.h
+++ opendbc/safety/safety/safety_hyundai.h
@@ -205,7 +205,7 @@ static void hyundai_rx_hook(const CANPacket_t *to_push) {
 static bool hyundai_tx_hook(const CANPacket_t *to_send) {
   const TorqueSteeringLimits HYUNDAI_STEERING_LIMITS = HYUNDAI_LIMITS(404, 4, 7);
   const TorqueSteeringLimits HYUNDAI_STEERING_LIMITS_ALT = HYUNDAI_LIMITS(404, 3, 7);
-  const TorqueSteeringLimits HYUNDAI_STEERING_LIMITS_ALT_2 = HYUNDAI_LIMITS(404, 3, 7);
+  const TorqueSteeringLimits HYUNDAI_STEERING_LIMITS_ALT_2 = HYUNDAI_LIMITS(400, 3, 7);
 
   bool tx = true;
   int addr = GET_ADDR(to_send);
